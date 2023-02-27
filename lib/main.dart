65dbd4c38373cdf815e1fc81ece59bab8f0cb83a
Jparkbro <hyeongjoopark0324@gmail.com>
2023-02-27 09:32:22 +0900

    splash screen

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(SplashScreen());
+}
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            color: Color(0xfff99231),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset('assets/logo.png', width: 200,),
+                    CircularProgressIndicator(
+                      valueColor: AlwaysStoppedAnimation(
+                        Colors.white
+                      ),
+                    ),
+                  ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
